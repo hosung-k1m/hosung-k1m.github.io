4e6b16b1f521b0ef8616621d58ebbfcd9a1f1014
Hosung Kim <hk63@rice.edu>
2025-05-13 19:08:27 -0400

    deplot

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
+  base: '/',  // Not '/hosung-kim11.github.io/' since this is a user/org site
 })
